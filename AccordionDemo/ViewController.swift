b3b2c9da4f012934f74755df6e9f387ff9ec421e
Dominic Drees <dominic.drees@atino.de>
2020-10-10 09:26:43 +0200

    Initial Commit

---
+++ AccordionDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AccordionDemo
+//
+//  Created by Dominic Drees on 10.10.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
